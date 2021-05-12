80cb3ff01ba335c244d96e015115435a6a800c3f
zamzam90 <siltanen.sami@hotmail.com>
2021-05-12 15:39:18 +0300

    kommentointia, css muokkausta

--- imgGalleryTest/script.js
+++ imgGalleryTest/script.js
@@ -1,6 +1,13 @@
+/* kuvagalleria testailua 
+TODO:
+    -kun kuvaa klikkaa se aukeaa lightboxiin? https://www.w3schools.com/howto/howto_js_lightbox.asp
+        tai modaliin? https://www.w3schools.com/howto/howto_css_modal_images.asp
+
+*/
+
 /* Header greeter juttu */
-var myDate = new Date();
-var hrs = myDate.getHours();
+var myDate = new Date(); //haetaan päivämäärä muuttujaan
+var hrs = myDate.getHours(); //haetaan muuttujasta tunnit
 var greet;
 if (hrs < 12) greet = "Hyvää huomenta!";
 else if (hrs >= 12 && hrs <= 17) greet = "Hyvää päivää";
@@ -8,6 +15,8 @@ else if (hrs >= 17 && hrs <= 24) greet = "Hyvää iltaa";
 
 document.getElementById("greeter").innerHTML =
   greet + " tervetuloa kuvagalleriaan!";
+
+/* greeter stuff end */
 
 /* automaaginen slideshow
     https://www.w3schools.com/howto/howto_js_slideshow.asp
@@ -17,14 +26,16 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("slide");
+  var slides = document.getElementsByClassName("slide"); // haetaan kaikki slide luokan omaavat elementit
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; // laitetaan jokaisen elementin style noneksi=piilotetaan
   }
-  slideIndex++;
+  slideIndex++; // kasvatetaan slide indexiä
   if (slideIndex > slides.length) {
+    //jos slide index yli kuvien määrän, asetetaan se ykköseksi
     slideIndex = 1;
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block"; // vaihdetaan slide elementin style blockiksi = näytetään kuva
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+/* slideshow stuff end */
